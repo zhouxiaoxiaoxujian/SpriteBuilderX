2810c88995b4b7695e29296a70836936796487c2
Sergey <non_2001@mail.ru>
2014-06-29 21:19:56 +0400

    TextField Improvements

--- SpriteBuilder/CCTextField/CCBPTextField.h
+++ SpriteBuilder/CCTextField/CCBPTextField.h
@@ -10,4 +10,26 @@
 
 @interface CCBPTextField : CCTextField
 
+/** The platform font to use for the text. */
+@property (nonatomic,strong) NSString* fontName;
+
+/** The color of the text (If not using shadow or outline). */
+@property (nonatomic,strong) CCColor* fontColor;
+
+/** The platform font to use for the text. */
+@property (nonatomic,strong) NSString* placeholderFontName;
+
+/** The font size of the text. */
+@property (nonatomic,assign) float placeholderFontSize;
+
+/** The color of the text (If not using shadow or outline). */
+@property (nonatomic,strong) CCColor* placeholderFontColor;
+
+@property (nonatomic,strong) NSString* placeholder;
+
+@property (nonatomic,assign) int inputFlag;
+@property (nonatomic,assign) int keyboardReturnType;
+@property (nonatomic,assign) int inputMode;
+@property (nonatomic,assign) int maxLength;
+
 @end
