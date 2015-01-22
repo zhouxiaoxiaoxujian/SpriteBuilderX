fe1ef9eaa03eed353c783fe41b822aa85ed3f370
Sergey <non_2001@mail.ru>
2015-01-22 18:15:48 +0300

    Layout box use visibility of child

--- SpriteBuilder/CCLayoutBox/CCBPLayoutBox.m
+++ SpriteBuilder/CCLayoutBox/CCBPLayoutBox.m
@@ -50,6 +50,90 @@
     return ret;
 }
 
+static float roundUpToEven(float f)
+{
+    return ceilf(f/2.0f) * 2.0f;
+}
+
+- (void) layout
+{
+    [super layout];
+    if (self.direction == CCLayoutBoxDirectionHorizontal)
+    {
+        // Get the maximum height
+        float maxHeight = 0;
+        for (CCNode* child in self.children)
+        {
+            float height = child.contentSizeInPoints.height;
+            if (height > maxHeight) maxHeight = height;
+        }
+        
+        // Position the nodes
+        float width = 0;
+        for (CCNode* child in self.children)
+        {
+            if(child.visible)
+            {
+                CGSize childSize = child.contentSizeInPoints;
+                
+                CGPoint offset = child.anchorPointInPoints;
+                CGPoint localPos = ccp(roundf(width), roundf((maxHeight-childSize.height)/2.0f));
+                CGPoint position = ccpAdd(localPos, offset);
+                
+                child.position = position;
+                child.positionType = CCPositionTypePoints;
+                
+                width += childSize.width;
+                width += self.spacing;
+            }
+        }
+        
+        // Account for last added increment
+        width -= self.spacing;
+        if (width < 0) width = 0;
+        
+        self.contentSizeType = CCSizeTypePoints;
+        self.contentSize = CGSizeMake(roundUpToEven(width), roundUpToEven(maxHeight));
+    }
+    else
+    {
+        // Get the maximum width
+        float maxWidth = 0;
+        for (CCNode* child in self.children)
+        {
+            float width = child.contentSizeInPoints.width;
+            if (width > maxWidth) maxWidth = width;
+        }
+        
+        // Position the nodes
+        float height = 0;
+        for (CCNode* child in self.children)
+        {
+            if(child.visible)
+            {
+                CGSize childSize = child.contentSizeInPoints;
+                
+                CGPoint offset = child.anchorPointInPoints;
+                CGPoint localPos = ccp(roundf((maxWidth-childSize.width)/2.0f), roundf(height));
+                CGPoint position = ccpAdd(localPos, offset);
+                
+                child.position = position;
+                child.positionType = CCPositionTypePoints;
+                
+                height += childSize.height;
+                height += self.spacing;
+            }
+        }
+        
+        // Account for last added increment
+        height -= self.spacing;
+        if (height < 0) height = 0;
+        
+        self.contentSizeType = CCSizeTypePoints;
+        self.contentSize = CGSizeMake(roundUpToEven(maxWidth), roundUpToEven(height));
+    }
+}
+
 -(void)visit:(CCRenderer *)renderer parentTransform:(const GLKMatrix4 *)parentTransform
 {
     if(_clipContent)
