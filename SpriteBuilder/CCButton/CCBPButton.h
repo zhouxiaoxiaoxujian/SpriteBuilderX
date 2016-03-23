d2453ca7a7e47349438d076434400eed66a87ca9
Sergey <non_2001@mail.ru>
2016-03-23 20:02:23 +0300

    Button imageScale & children

--- SpriteBuilder/CCButton/CCBPButton.h
+++ SpriteBuilder/CCButton/CCBPButton.h
@@ -6,10 +6,174 @@
 //
 //
 
-#import "CCButton.h"
+#import "CCProtectedNode.h"
+#import "cocos2d.h"
 
-@interface CCBPButton : CCButton
--(void)onSetSizeFromTexture;
+/**
+ The possible states for a CCControl.
+ */
+typedef NS_ENUM(NSUInteger, CCBPControlState)
+{
+    /** The normal, or default state of a control — that is, enabled but neither selected nor highlighted. */
+    CCBPControlStateNormal       = 0,
+    
+    /** Highlighted state of a control. A control enters this state when a touch down, drag inside or drag enter is performed. You can retrieve and set this value through the highlighted property. */
+    CCBPControlStateHighlighted  = 1,
+    
+    /** Disabled state of a control. This state indicates that the control is currently disabled. You can retrieve and set this value through the enabled property. */
+    CCBPControlStateDisabled     = 2,
+};
+
+@interface CCBPButton : CCProtectedNode
+{
+    NSMutableDictionary* _backgroundSpriteFrames;
+    NSMutableDictionary* _backgroundColors;
+    NSMutableDictionary* _backgroundOpacities;
+    NSMutableDictionary* _labelColors;
+    NSMutableDictionary* _labelOpacities;
+    float _originalScaleX;
+    float _originalScaleY;
+    
+    float _originalHitAreaExpansion;
+}
+
+@property (nonatomic,readonly) CCSprite9Slice* background;
+@property (nonatomic,readonly) CCLabelTTF* label;
+@property (nonatomic,assign) float horizontalPadding;
+@property (nonatomic,assign) float verticalPadding;
+@property (nonatomic,strong) NSString* title;
+@property (nonatomic,assign) BOOL togglesSelectedState;
+
+/// -----------------------------------------------------------------------
+/// @name Creating Buttons
+/// -----------------------------------------------------------------------
+
+/**
+ *  Initializes a new button with a title and no background. Uses default font and font size.
+ *
+ *  @param title The title text of the button.
+ *
+ *  @return A new button.
+ */
+- (id) initWithTitle:(NSString*) title;
+
+/**
+ *  Initializes a new button with a title and no background.
+ *
+ *  @param title    The title text of the button.
+ *  @param fontName Name of the TTF font to use for the title label.
+ *  @param size     Font size for the title label.
+ *
+ *  @return A new button.
+ */
+- (id) initWithTitle:(NSString *)title fontName:(NSString*)fontName fontSize:(float)size;
+
+/**
+ *  Initializes a new button with the specified title for the label and sprite frame for its background.
+ *
+ *  @param title       The title text of the button.
+ *  @param spriteFrame Stretchable background image.
+ *
+ *  @return A new button.
+ */
+- (id) initWithTitle:(NSString*) title spriteFrame:(CCSpriteFrame*) spriteFrame;
+
+/**
+ *  Initializes a new button with the speicified title for the label, sprite frames for its background in different states.
+ *
+ *  @param title       The title text of the button.
+ *  @param spriteFrame Stretchable background image for the normal state.
+ *  @param highlighted Stretchable background image for the highlighted state.
+ *  @param disabled    Stretchable background image for the disabled state.
+ *
+ *  @return A new button.
+ */
+- (id) initWithTitle:(NSString*) title spriteFrame:(CCSpriteFrame*) spriteFrame highlightedSpriteFrame:(CCSpriteFrame*) highlighted disabledSpriteFrame:(CCSpriteFrame*) disabled;
+
+/**
+ *  Sets the background color for the specified state. The color is multiplied into the background sprite frame.
+ *
+ *  @param color Color applied to background image.
+ *  @param state State to apply the color to.
+ */
+- (void) setBackgroundColor:(CCColor*) color forState:(CCBPControlState) state;
+
+/**
+ *  Gets the background color for the specified state.
+ *
+ *  @param state State to get the color for.
+ *
+ *  @return Background color.
+ */
+- (CCColor*) backgroundColorForState:(CCBPControlState)state;
+
+/**
+ *  Sets the background's opacity for the specified state.
+ *
+ *  @param opacity Opacity to apply to the background image
+ *  @param state   State to apply the opacity to.
+ */
+- (void) setBackgroundOpacity:(CGFloat) opacity forState:(CCBPControlState) state;
+
+/**
+ *  Gets the background opacity for the specified state.
+ *
+ *  @param state State to get the opacity for.
+ *
+ *  @return Opacity.
+ */
+- (CGFloat) backgroundOpacityForState:(CCBPControlState)state;
+
+/**
+ *  Sets the label's color for the specified state.
+ *
+ *  @param color Color applied to the label.
+ *  @param state State to set the color for.
+ */
+- (void) setLabelColor:(CCColor*) color forState:(CCBPControlState) state;
+
+/**
+ *  Gets the label's color for the specified state.
+ *
+ *  @param state State to get the color for.
+ *
+ *  @return Label color.
+ */
+- (CCColor*) labelColorForState:(CCBPControlState) state;
+
+/**
+ *  Sets the label's opacity for the specified state.
+ *
+ *  @param opacity Opacity applied to the label.
+ *  @param state   State to set the opacity for.
+ */
+- (void) setLabelOpacity:(CGFloat) opacity forState:(CCBPControlState) state;
+
+/**
+ *  Gets the label's opacity for the specified state.
+ *
+ *  @param state State to get the opacity for.
+ *
+ *  @return Label opacity.
+ */
+- (CGFloat) labelOpacityForState:(CCBPControlState) state;
+
+/**
+ *  Sets the background's sprite frame for the specified state. The sprite frame will be stretched to the preferred size of the label. If set to `NULL` no background will be drawn.
+ *
+ *  @param spriteFrame Sprite frame to use for drawing the background.
+ *  @param state       State to set the background for.
+ */
+- (void) setBackgroundSpriteFrame:(CCSpriteFrame*)spriteFrame forState:(CCBPControlState)state;
+
+/**
+ *  Gets the background's sprite frame for the specified state.
+ *
+ *  @param state State to get the sprite frame for.
+ *
+ *  @return Background sprite frame.
+ */
+- (CCSpriteFrame*) backgroundSpriteFrameForState:(CCBPControlState)state;
 
 /** Sets the left margin exclusively. */
 @property (nonatomic, assign) float marginLeft;
@@ -45,5 +209,9 @@
 @property (nonatomic,assign) float rightPadding;
 @property (nonatomic,assign) float topPadding;
 @property (nonatomic,assign) float bottomPadding;
+
+@property (nonatomic,assign) CCBPControlState state;
+
+@property (nonatomic,assign) CGFloat imageScale;
 
 @end
--- SpriteBuilder/CCButton/CCBPButton.m
+++ SpriteBuilder/CCButton/CCBPButton.m
@@ -14,78 +14,109 @@
 #import "CCSprite9Slice.h"
 #import "PositionPropertySetter.h"
 
+#define kCCFatFingerExpansion 70
+
 @implementation CCBPButton
 
 - (id) init
 {
-    self = [super init];
-    if (!self) return NULL;
-    
-    self.userInteractionEnabled = NO;
-    self.zoomOnClick = 1.0f;
-    
-    _adjustsFontSizeToFit = YES;
+    return [self initWithTitle:@"" spriteFrame:NULL];
+}
+
+- (id) initWithTitle:(NSString *)title
+{
+    self = [self initWithTitle:title spriteFrame:NULL highlightedSpriteFrame:NULL disabledSpriteFrame:NULL];
     
     return self;
 }
 
-/*- (void) setContentSizeType:(CCSizeType)contentSizeType
+- (id) initWithTitle:(NSString *)title fontName:(NSString*)fontName fontSize:(float)size
 {
-    [self setPreferredSizeType:contentSizeType];
+    self = [self initWithTitle:title];
+    self.label.fontName = fontName;
+    self.label.fontSize = size;
+    
+    return self;
 }
 
-- (CCSizeType) contentSizeType
+- (id) initWithTitle:(NSString*) title spriteFrame:(CCSpriteFrame*) spriteFrame
 {
-    return self.preferredSizeType;
-}*/
-
-- (NSArray*) keysForwardedToLabel
-{
-    return @[@"fontName",
-             @"fontColor",
-             @"outlineColor",
-             @"outlineWidth",
-             @"shadowColor",
-             @"shadowBlurRadius",
-             @"shadowOffset",
-             @"shadowOffsetType",
-             @"horizontalAlignment",
-             @"verticalAlignment"];
+    self = [self initWithTitle:title spriteFrame:spriteFrame highlightedSpriteFrame:NULL disabledSpriteFrame:NULL];
+    
+    // Setup default colors for when only one frame is used
+    [self setBackgroundColor:[CCColor colorWithWhite:0.7 alpha:1] forState:CCBPControlStateHighlighted];
+    [self setLabelColor:[CCColor colorWithWhite:0.7 alpha:1] forState:CCBPControlStateHighlighted];
+    
+    [self setBackgroundOpacity:0.5f forState:CCBPControlStateDisabled];
+    [self setLabelOpacity:0.5f forState:CCBPControlStateDisabled];
+    
+    return self;
 }
 
-- (void) setFontSize:(CGFloat)fontSize
+- (id) initWithTitle:(NSString*) title spriteFrame:(CCSpriteFrame*) spriteFrame highlightedSpriteFrame:(CCSpriteFrame*) highlighted disabledSpriteFrame:(CCSpriteFrame*) disabled
 {
-    _fontSize = fontSize;
-    _needsLayout = true;
-}
-
-- (void) setLeftPadding:(float)leftPadding
-{
-    _leftPadding = leftPadding;
-    _needsLayout = true;
-}
-
-- (void) setRightPadding:(float)rightPadding
-{
-    _rightPadding = rightPadding;
-    _needsLayout = true;
-}
-
-- (void) setTopPadding:(float)topPadding
-{
-    _topPadding = topPadding;
-    _needsLayout = true;
-}
-
-- (void) setBottomPadding:(float)bottomPadding
-{
-    _bottomPadding = bottomPadding;
-    _needsLayout = true;
+    self = [super init];
+    if (!self) return NULL;
+    
+    self.anchorPoint = ccp(0.5f, 0.5f);
+    _imageScale = 1.0f;
+    
+    if (!title) title = @"";
+    _state = CCBPControlStateNormal;
+    
+    // Setup holders for properties
+    _backgroundColors = [NSMutableDictionary dictionary];
+    _backgroundOpacities = [NSMutableDictionary dictionary];
+    _backgroundSpriteFrames = [NSMutableDictionary dictionary];
+    
+    _labelColors = [NSMutableDictionary dictionary];
+    _labelOpacities = [NSMutableDictionary dictionary];
+    
+    _zoomOnClick = 1.0f;
+    
+    // Setup background image
+    if (spriteFrame)
+    {
+        _background = [CCSprite9Slice spriteWithSpriteFrame:spriteFrame];
+        [self setBackgroundSpriteFrame:spriteFrame forState:CCBPControlStateNormal];
+    }
+    else
+    {
+        _background = [[CCSprite9Slice alloc] init];
+    }
+    
+    if (highlighted)
+    {
+        [self setBackgroundSpriteFrame:highlighted forState:CCBPControlStateHighlighted];
+    }
+    
+    if (disabled)
+    {
+        [self setBackgroundSpriteFrame:disabled forState:CCBPControlStateDisabled];
+    }
+    
+    [self addProtectedChild:_background z:0];
+    
+    // Setup label
+    _label = [CCLabelTTF labelWithString:title fontName:@"Helvetica" fontSize:14];
+    _label.adjustsFontSizeToFit = NO;
+    _label.horizontalAlignment = CCTextAlignmentCenter;
+    _label.verticalAlignment = CCVerticalTextAlignmentCenter;
+    
+    [self addProtectedChild:_label z:1];
+    
+    // Setup original scale
+    _originalScaleX = _originalScaleY = 1;
+    
+    [self needsLayout];
+    [self stateChanged];
+    
+    return self;
 }
 
 - (void) layout
 {
-    CGSize contentSize = [self convertContentSizeToPoints:self.preferredSize type:self.contentSizeType];
+    CGSize contentSize = [self convertContentSizeToPoints:self.contentSize type:self.contentSizeType];
     CGSize paddedLabelSize = CGSizeMake(contentSize.width - (self.leftPadding + self.rightPadding), contentSize.height -  (self.topPadding + self.bottomPadding));
     
     if(_adjustsFontSizeToFit && paddedLabelSize.width>0 && paddedLabelSize.height>0)
@@ -127,7 +158,7 @@
     
     self.label.dimensions = paddedLabelSize;
     
-    self.background.contentSize = contentSize;
+    [_background setContentSize:CGSizeMake(contentSize.width / _imageScale, contentSize.height / _imageScale)];
     self.background.anchorPoint = ccp(0.5f,0.5f);
     self.background.positionType = CCPositionTypeNormalized;
     self.background.position = ccp(0.5f,0.5f);
@@ -136,30 +167,307 @@
     self.label.anchorPoint = ccp(0, 0);
     self.label.position = ccp(self.leftPadding, self.bottomPadding);
     
-    _needsLayout = NO;
+    [super layout];
 }
 
--(void) setAdjustsFontSizeToFit:(BOOL)value
+- (void) updatePropertiesForState:(CCBPControlState)state
 {
-    _adjustsFontSizeToFit = value;
-    _needsLayout = YES;
+    // Update background
+    ccColor4F backgroundColor = [self backgroundColorForState:state].ccColor4f;
+    backgroundColor.r *= _displayColor.r;
+    backgroundColor.g *= _displayColor.g;
+    backgroundColor.b *= _displayColor.b;
+    _background.color = [CCColor colorWithCcColor4f:backgroundColor];
+    _background.opacity = [self backgroundOpacityForState:state] * _displayColor.a;
+    
+    CCSpriteFrame* spriteFrame = [self backgroundSpriteFrameForState:state];
+    if (!spriteFrame) spriteFrame = [self backgroundSpriteFrameForState:CCBPControlStateNormal];
+    _background.spriteFrame = spriteFrame;
+    
+    // Update label
+    ccColor4F labelColor = [self labelColorForState:state].ccColor4f;
+    labelColor.r *= _displayColor.r;
+    labelColor.g *= _displayColor.g;
+    labelColor.b *= _displayColor.b;
+    _label.color = [CCColor colorWithCcColor4f:labelColor];;
+    _label.opacity = [self labelOpacityForState:state] * _displayColor.a;
+    
+    [self needsLayout];
 }
 
--(void) setContentSize:(CGSize)size
+- (void) stateChanged
 {
-    [self setPreferredSize:size];
-    [self setMaxSize:size];
-    [super setContentSize:size];
+    switch (_state) {
+        case CCBPControlStateNormal:
+            _label.scaleX = _originalScaleX;
+            _label.scaleY = _originalScaleY;
+            _background.scaleX = _originalScaleX * _imageScale;
+            _background.scaleY = _originalScaleY * _imageScale;
+            [self updatePropertiesForState:CCBPControlStateNormal];
+            break;
+            
+        case CCBPControlStateHighlighted:
+            _label.scaleX = _originalScaleX * _zoomOnClick;
+            _label.scaleY = _originalScaleY * _zoomOnClick;
+            _background.scaleX = _originalScaleX * _zoomOnClick * _imageScale;
+            _background.scaleY = _originalScaleY * _zoomOnClick * _imageScale;
+            [self updatePropertiesForState:CCBPControlStateHighlighted];
+            break;
+            
+        case CCBPControlStateDisabled:
+            _label.scaleX = _originalScaleX;
+            _label.scaleY = _originalScaleY;
+            _background.scaleX = _originalScaleX * _imageScale;
+            _background.scaleY = _originalScaleY * _imageScale;
+            [self updatePropertiesForState:CCBPControlStateDisabled];
+            break;
+            
+        default:
+            break;
+    }
 }
 
--(CGSize) contentSize
+#pragma mark Properties
+
+- (void) setLabelColor:(CCColor*)color forState:(CCBPControlState)state
 {
-    return self.preferredSize;
+    [_labelColors setObject:color forKey:[NSNumber numberWithInt:state]];
+    [self stateChanged];
+}
+
+- (CCColor*) labelColorForState:(CCBPControlState)state
+{
+    CCColor* color = [_labelColors objectForKey:[NSNumber numberWithInt:state]];
+    if (!color) color = [CCColor whiteColor];
+    return color;
+}
+
+- (void) setLabelOpacity:(CGFloat)opacity forState:(CCBPControlState)state
+{
+    [_labelOpacities setObject:[NSNumber numberWithFloat:opacity] forKey:[NSNumber numberWithInt:state]];
+    [self stateChanged];
+}
+
+- (CGFloat) labelOpacityForState:(CCBPControlState)state
+{
+    NSNumber* val = [_labelOpacities objectForKey:[NSNumber numberWithInt:state]];
+    if (!val) return 1;
+    return [val floatValue];
+}
+
+- (void) setBackgroundColor:(CCColor*)color forState:(CCBPControlState)state
+{
+    [_backgroundColors setObject:color forKey:[NSNumber numberWithInt:state]];
+    [self stateChanged];
+}
+
+- (CCColor*) backgroundColorForState:(CCBPControlState)state
+{
+    CCColor* color = [_backgroundColors objectForKey:[NSNumber numberWithInt:state]];
+    if (!color) color = [CCColor whiteColor];
+    return color;
+}
+
+- (void) setBackgroundOpacity:(CGFloat)opacity forState:(CCBPControlState)state
+{
+    [_backgroundOpacities setObject:[NSNumber numberWithFloat:opacity] forKey:[NSNumber numberWithInt:state]];
+    [self stateChanged];
+}
+
+- (CGFloat) backgroundOpacityForState:(CCBPControlState)state
+{
+    NSNumber* val = [_backgroundOpacities objectForKey:[NSNumber numberWithInt:state]];
+    if (!val) return 1;
+    return [val floatValue];
+}
+
+- (void) setBackgroundSpriteFrame:(CCSpriteFrame*)spriteFrame forState:(CCBPControlState)state
+{
+    if (spriteFrame)
+    {
+        [_backgroundSpriteFrames setObject:spriteFrame forKey:[NSNumber numberWithInt:state]];
+    }
+    else
+    {
+        [_backgroundSpriteFrames removeObjectForKey:[NSNumber numberWithInt:state]];
+    }
+    [self stateChanged];
+}
+
+- (CCSpriteFrame*) backgroundSpriteFrameForState:(CCBPControlState)state
+{
+    return [_backgroundSpriteFrames objectForKey:[NSNumber numberWithInt:state]];
+}
+
+- (void) setTitle:(NSString *)title
+{
+    _label.string = title;
+    [self needsLayout];
+}
+
+- (NSString*) title
+{
+    return _label.string;
+}
+
+- (void) setHorizontalPadding:(float)horizontalPadding
+{
+    _horizontalPadding = horizontalPadding;
+    [self needsLayout];
+}
+
+- (void) setVerticalPadding:(float)verticalPadding
+{
+    _verticalPadding = verticalPadding;
+    [self needsLayout];
+}
+
+- (void) setState:(CCBPControlState)state
+{
+    _state = state;
+    [self stateChanged];
+}
+
+- (void) setZoomOnClick:(float)zoomOnClick
+{
+    _zoomOnClick = zoomOnClick;
+    [self stateChanged];
+}
+
+- (void) setImageScale:(CGFloat) imageScale
+{
+    _imageScale = imageScale;
+    [self stateChanged];
+    [self needsLayout];
+}
+
+#pragma mark Setting properties by name
+
+- (CCBPControlState) controlStateFromString:(NSString*)stateName
+{
+    CCBPControlState state = 0;
+    if ([stateName isEqualToString:@"Normal"]) state = CCBPControlStateNormal;
+    else if ([stateName isEqualToString:@"Highlighted"]) state = CCBPControlStateHighlighted;
+    else if ([stateName isEqualToString:@"Disabled"]) state = CCBPControlStateDisabled;
+    
+    return state;
+}
+
+- (NSArray*) keysForwardedToLabel
+{
+    return @[@"fontName",
+             @"fontSize",
+             @"fontColor",
+             @"outlineColor",
+             @"outlineWidth",
+             @"shadowColor",
+             @"shadowBlurRadius",
+             @"shadowOffset",
+             @"shadowOffsetType",
+             @"horizontalAlignment",
+             @"verticalAlignment"];
+}
+
+- (void) setValue:(id)value forKey:(NSString *)key
+{
+    if ([[self keysForwardedToLabel] containsObject:key])
+    {
+        [_label setValue:value forKey:key];
+        [self needsLayout];
+        return;
+    }
+    NSRange separatorRange = [key rangeOfString:@"|"];
+    NSUInteger separatorLoc = separatorRange.location;
+    
+    if (separatorLoc == NSNotFound)
+    {
+        [super setValue:value forKey:key];
+        return;
+    }
+    
+    NSString* propName = [key substringToIndex:separatorLoc];
+    NSString* stateName = [key substringFromIndex:separatorLoc+1];
+    
+    CCBPControlState state = [self controlStateFromString:stateName];
+    
+    [self setValue:value forKey:propName state:state];
+}
+
+- (id) valueForKey:(NSString *)key
+{
+    if ([[self keysForwardedToLabel] containsObject:key])
+    {
+        return [_label valueForKey:key];
+    }
+    NSRange separatorRange = [key rangeOfString:@"|"];
+    NSUInteger separatorLoc = separatorRange.location;
+    
+    if (separatorLoc == NSNotFound)
+    {
+        return [super valueForKey:key];
+    }
+    
+    NSString* propName = [key substringToIndex:separatorLoc];
+    NSString* stateName = [key substringFromIndex:separatorLoc+1];
+    
+    CCBPControlState state = [self controlStateFromString:stateName];
+    
+    return [self valueForKey:propName state:state];
+}
+
+- (void) setValue:(id)value forKey:(NSString *)key state:(CCBPControlState)state
+{
+    if ([key isEqualToString:@"labelOpacity"])
+    {
+        [self setLabelOpacity:[value floatValue] forState:state];
+    }
+    else if ([key isEqualToString:@"labelColor"])
+    {
+        [self setLabelColor:value forState:state];
+    }
+    else if ([key isEqualToString:@"backgroundOpacity"])
+    {
+        [self setBackgroundOpacity:[value floatValue] forState:state];
+    }
+    else if ([key isEqualToString:@"backgroundColor"])
+    {
+        [self setBackgroundColor:value forState:state];
+    }
+    else if ([key isEqualToString:@"backgroundSpriteFrame"])
+    {
+        [self setBackgroundSpriteFrame:value forState:state];
+    }
+}
+
+- (id) valueForKey:(NSString *)key state:(CCBPControlState)state
+{
+    if ([key isEqualToString:@"labelOpacity"])
+    {
+        return [NSNumber numberWithFloat:[self labelOpacityForState:state]];
+    }
+    else if ([key isEqualToString:@"labelColor"])
+    {
+        return [self labelColorForState:state];
+    }
+    else if ([key isEqualToString:@"backgroundOpacity"])
+    {
+        return [NSNumber numberWithFloat:[self backgroundOpacityForState:state]];
+    }
+    else if ([key isEqualToString:@"backgroundColor"])
+    {
+        return [self backgroundColorForState:state];
+    }
+    else if ([key isEqualToString:@"backgroundSpriteFrame"])
+    {
+        return [self backgroundSpriteFrameForState:state];
+    }
+    
+    return NULL;
 }
 
 -(void)onSetSizeFromTexture
 {
-    CCSpriteFrame * spriteFrame = _backgroundSpriteFrames[@(CCControlStateNormal)];
+    CCSpriteFrame * spriteFrame = _backgroundSpriteFrames[@(CCBPControlStateNormal)];
     if(spriteFrame == nil)
         return;
     
@@ -167,12 +475,11 @@
     
     self.contentSize = spriteFrame.texture.contentSize;
     self.contentSizeType = CCSizeTypeMake(CCSizeUnitPoints, CCSizeUnitPoints);
-    self.maxSize = self.preferredSize;
-    self.maxSizeType = self.preferredSizeType;
     
     [self willChangeValueForKey:@"contentSize"];
     [self didChangeValueForKey:@"contentSize"];
     [[InspectorController sharedController] refreshProperty:@"contentSize"];
+    [super needsLayout];
 }
 
 - (void)setMarginLeft:(float)marginLeft
@@ -249,4 +556,53 @@
     return self.background.marginTop;
 }
 
+-(void) setAdjustsFontSizeToFit:(BOOL)value
+{
+    _adjustsFontSizeToFit = value;
+    [self needsLayout];
+}
+
+- (void) setFontSize:(CGFloat)fontSize
+{
+    _fontSize = fontSize;
+    [self needsLayout];
+}
+
+- (void) setLeftPadding:(float)leftPadding
+{
+    _leftPadding = leftPadding;
+    [self needsLayout];
+}
+
+- (void) setRightPadding:(float)rightPadding
+{
+    _rightPadding = rightPadding;
+    [self needsLayout];
+}
+
+- (void) setTopPadding:(float)topPadding
+{
+    _topPadding = topPadding;
+    [self needsLayout];
+}
+
+- (void) setBottomPadding:(float)bottomPadding
+{
+    _bottomPadding = bottomPadding;
+    [self needsLayout];
+}
+
+- (void) setContentSize:(CGSize) contentSize
+{
+    [super setContentSize:contentSize];
+    [self needsLayout];
+}
+
+- (void) setContentSizeType:(CCSizeType)contentSizeType
+{
+    [super setContentSizeType:contentSizeType];
+    [self needsLayout];
+}
+
 @end
+
