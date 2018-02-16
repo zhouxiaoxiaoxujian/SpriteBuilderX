5d40bfd940cad87eb1455e17918a3ed9f9fbc2eb
Volodymyr Klymenko <>
2018-02-16 14:51:09 +0200

    Misc. change, also mainMenu changes by Xcode(when old xib item selected it renews autoresizingMask key value, so I've seleted them all to make it finally happened).

--- SpriteBuilder/ccBuilder/CocosScene.m
+++ SpriteBuilder/ccBuilder/CocosScene.m
@@ -2248,7 +2248,7 @@ static NSString * kZeroContentSizeImage = @"sel-round.png";
     CCBDocument *curDoc = appDelegate.currentDocument;
     [curDoc.stageScrollOffsets setValue:@(scrollOffset.x) forKey:[NSString stringWithFormat:@"offset_x_%d",curDoc.currentResolution]];
     [curDoc.stageScrollOffsets setValue:@(scrollOffset.y) forKey:[NSString stringWithFormat:@"offset_y_%d",curDoc.currentResolution]];
-    [appDelegate.currentDocument.stageZooms setValue:@(self.stageZoom) forKey:[NSString stringWithFormat:@"zoom_%d",curDoc.currentResolution]];
+    [curDoc.stageZooms setValue:@(self.stageZoom) forKey:[NSString stringWithFormat:@"zoom_%d",curDoc.currentResolution]];
     
 }
 
